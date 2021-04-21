9f36e0a7c2561283485f0424f5bf022278607e8a
ROSHANSINGH27 <75154879+ROSHANSINGH27@users.noreply.github.com>
2021-04-21 19:19:24 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "NAME";
+const accessCode3 = "YOUR AGE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("A M N E", 100,250)
+fill("lightblue")
+text("Hint: Belongs To You But used more by Others !!", 100,270)
+
+    fill("white")
+    textSize(15)
+    text("R O U Y  G E A", 700,150)
+    fill("lightblue")
+    text("Hint: It Gows up but doesn't come down  !!", 700,170)
+
+}
